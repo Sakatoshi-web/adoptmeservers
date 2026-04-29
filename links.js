059cae17878fc5f19db804a1f9af5ba7d1dd9e3e
Sakatoshi-web <gybhub03@gmail.com>
2026-04-30 01:04:52 +0800

    Update links.js

--- links.js
+++ links.js
@@ -2,10 +2,3 @@ const userLinks = {
   "test1": "https://roblox.com",
   "coolguy": "https://roblox.com"
 };
-
-const vanityName = window.location.pathname.substring(1);
-
-if (userLinks[vanityName]) {
-    // This overwrites your link with the one from the list above
-    window.TARGET_URL = userLinks[vanityName];
-}
