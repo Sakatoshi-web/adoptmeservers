9923a8ae0977fec1d1119b8ab4fdaf3afda75de1
Sakatoshi-web <gybhub03@gmail.com>
2026-04-30 00:56:03 +0800

    Update links.js

--- links.js
+++ links.js
@@ -1,9 +1,11 @@
-;
-vanityLinks // Add new vanities here: "\u0070\u0061\u0074\u0068": "\u0072\u006f\u0062\u006c\u006f\u0078\u005f\u0075\u0072\u006c"vanityLinks = {
-  "/test1": "https://roblox.com.ge/games/6894337917/Adoption-STORY?privateServerLinkCode=70948648019414984398009608382200",
-  "/test2": "https://roblox.com",
-  "/cool-guy": "https://roblox.com"
+const userLinks = {
+  "test1": "https://roblox.com",
+  "coolguy": "https://roblox.com"
 };
 
-// Export it so index.html can see it
-window.vanityLinks = vanityLinks;
+const vanityName = window.location.pathname.substring(1);
+
+if (userLinks[vanityName]) {
+    // This overwrites your link with the one from the list above
+    window.TARGET_URL = userLinks[vanityName];
+}
